c089f757cc3f32d5014ebe5f245416ca25ce4be6
Arpit Pramanik <95621058+ArpitPramanik7@users.noreply.github.com>
2024-06-20 15:13:21 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -1,9 +1,11 @@
 import java.util.*;
 public class mul{
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+        //Scanner sc =new Scanner(System.in);
+        //int a=sc.nextInt();
+        //int b=sc.nextInt();
+        int a=we;
+        int b=9;
         int m=a*b;
         System.out.println("Into = "+m);
     }
