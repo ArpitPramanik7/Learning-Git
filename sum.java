a755ef18ce8429be86ebc80de0e9e712c41f8e13
Arpit Pramanik <95621058+ArpitPramanik7@users.noreply.github.com>
2024-06-20 15:08:03 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,9 +1,11 @@
 import java.util.*;
 public class sum{
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+       // Scanner sc =new Scanner(System.in);
+       // int a=sc.nextInt();
+       // int b=sc.nextInt();
+        int a=11;
+        int b=4;
         int s=a+b;
         System.out.println("Sum = "+s);
     }
